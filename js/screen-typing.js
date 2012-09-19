5f300573ebeffd99fb74c7c6f5e7f9d8831d822e
Brian Fryer <brianfryer@gmail.com>
2012-09-19 18:37:56 -0500

    Got the on-screen keyboard, and physical keyboard inputs working as intended.

--- js/screen-typing.js
+++ js/screen-typing.js
@@ -1,39 +1,68 @@
+// "press" = you used your physical keyboard
+// "clicked" = you used your mouse to click the on-screen keyboard
+
 $(document).ready(function() {
 
-    // Find the textarea, and save it to var screen
+// Find the textarea, save it to var screen, and focus the cursor on it
     var screen = $("#screen > textarea");
+    screen.focus();
 
+// Listen for when a (non-modifier, or non-function) key is clicked
     $('li').not('.modifier, .short-key').click(function() {
+
         // Find the first <span>, get the contents, trim away the whitespace, and save it to var txt
-        var txt = $(this).find(':first-child').text().trim();
-        // Add the trimmed txt to the textarea
-        screen.val(screen.val() + txt);
+        var character = $(this).find(':first-child').text().trim();
+
+        // Extend jQuery to insert characters at the caret
+        jQuery.fn.extend({
+            insertAtCaret: function(character){
+                return this.each(function(i) {
+                    if (document.selection) {
+                        //For browsers like Internet Explorer
+                        this.focus();
+                        sel = document.selection.createRange();
+                        sel.text = character;
+                        this.focus();
+                    }
+                    else if (this.selectionStart || this.selectionStart == '0') {
+                        //For browsers like Firefox and Webkit based
+                        var startPos = this.selectionStart;
+                        var endPos = this.selectionEnd;
+                        var scrollTop = this.scrollTop;
+                        this.value = this.value.substring(0, startPos)+character+this.value.substring(endPos,this.value.length);
+                        this.focus();
+                        this.selectionStart = startPos + character.length;
+                        this.selectionEnd = startPos + character.length;
+                        this.scrollTop = scrollTop;
+                    } else {
+                        this.value += character;
+                        this.focus();
+                    }
+                })
+            }
+        });
+
+        // Insert characters in the textarea at the current caret
+        screen.insertAtCaret(character);
     });
 
-    var clickedKey = $(document).keydown( function(event){ String.fromCharCode(event.keyCode); });
-
-    KeyboardJS.bind.key(
-        // Physical keyboard input
-        clickedKey,
-        // onDownCallback
-        function() {
-            // Make the on-screen key flash
-            $('#' + clickedKey).addClass('hover');
-            // If the textarea has focus...
-            if ($('#screen > textarea').is(':focus')) {
-                // ...do nothing
-            } else {
-                // Add the trimmed txt from the first span to the textarea
-                var txt = $('.keys').find('#' + clickedKey).children(':first').text().trim();
-                screen.val(screen.val() + txt);
-            }
+    $(document).on({
+        // Do this when a key is pressed
+        'keydown': function(event){
+            // Get the value of the key being pressed and make sure it's lower case
+            key = (String.fromCharCode(event.keyCode)).toLowerCase();
+            // Make the on-screen key flash for 100ms
+            $('#' + key).addClass('hover');
+            // Focus on the textarea
+            $('#screen > textarea').focus();
         },
-        function() {
+        // Do this when a key is let go
+        'keyup': function(event) {
+            // Get the value of the key being pressed
+            key = String.fromCharCode(event.keyCode).toLowerCase();
             // After a key is clicked, remove the .hover class
-            setTimeout(function() {
-                $('.keys').find('#' + clickedKey).removeClass('hover');
-            }, 100);
+            $('#' + key).removeClass('hover').delay(100);
         }
-    );
+    });
 
 });
