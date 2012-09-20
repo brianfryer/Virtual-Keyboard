279216a9c28bf0322d1a75219c10a1988be032ea
Brian Fryer <brianfryer@gmail.com>
2012-09-19 21:32:55 -0500

    Making things work.

--- js/screen-typing.js
+++ js/screen-typing.js
@@ -3,73 +3,93 @@
 
 $(document).ready(function() {
 
+    // Extend jQuery to insert characters at the caret
+    jQuery.fn.extend({
+        insertAtCaret: function(character){
+            return this.each(function(i) {
+                if (document.selection) {
+                    //For browsers like Internet Explorer
+                    this.focus();
+                    sel = document.selection.createRange();
+                    sel.text = character;
+                    this.focus();
+                }
+                else if (this.selectionStart || this.selectionStart == '0') {
+                    //For browsers like Firefox and Webkit based
+                    var startPos = this.selectionStart;
+                    var endPos = this.selectionEnd;
+                    var scrollTop = this.scrollTop;
+                    this.value = this.value.substring(0, startPos)+character+this.value.substring(endPos,this.value.length);
+                    this.focus();
+                    this.selectionStart = startPos + character.length;
+                    this.selectionEnd = startPos + character.length;
+                    this.scrollTop = scrollTop;
+                } else {
+                    this.value += character;
+                    this.focus();
+                }
+            });
+        }
+    });
+
     // Find the textarea, save it to var screen, and focus the cursor on it
     var screen = $("#screen > textarea");
     screen.focus();
 
+    // Get the value of the key being clicked, and make sure it's trimmed
+    function getClickedKey(target) {
+        // Look for the contents of the first <span>, and add it to pressedKey
+        clickedKey = $(target).find(':first-child').text().trim();
+        //
+        console.log(clickedKey);
+        //
+        return clickedKey;
+    }
+
     // Listen for when a (non-modifier, or non-function) key is clicked
-    $('li').not('.modifier, .short-key').click(function() {
-
-        // Find the first <span>, get the contents, trim away the whitespace, and save it to var txt
-        var character = $(this).find(':first-child').text().trim();
-
-        // Extend jQuery to insert characters at the caret
-        jQuery.fn.extend({
-            insertAtCaret: function(character){
-                return this.each(function(i) {
-                    if (document.selection) {
-                        //For browsers like Internet Explorer
-                        this.focus();
-                        sel = document.selection.createRange();
-                        sel.text = character;
-                        this.focus();
-                    }
-                    else if (this.selectionStart || this.selectionStart == '0') {
-                        //For browsers like Firefox and Webkit based
-                        var startPos = this.selectionStart;
-                        var endPos = this.selectionEnd;
-                        var scrollTop = this.scrollTop;
-                        this.value = this.value.substring(0, startPos)+character+this.value.substring(endPos,this.value.length);
-                        this.focus();
-                        this.selectionStart = startPos + character.length;
-                        this.selectionEnd = startPos + character.length;
-                        this.scrollTop = scrollTop;
-                    } else {
-                        this.value += character;
-                        this.focus();
-                    }
-                });
-            }
-        });
-
-        // Insert characters in the textarea at the current caret
-        screen.insertAtCaret(character);
+    $('li').not('.modifier, .short-key').on({
+        'click': function() {
+            // Find the first <span>, get the contents, trim away the whitespace, and save it to var character
+            character = getClickedKey(this);
+            // Insert characters in the textarea at the caret
+            screen.insertAtCaret(character);
+        }
     });
 
-    // Get the value of the key being pressed and make sure it's lower case
-    function getCharacter(event) {
-
-        code = event.keyCode;
-
-        if ((code >= 65) && (code <= 90)) {
-            return (String.fromCharCode(code)).toLowerCase();
-        } else if (code == 16) {
+    // Get the value of the key being pressed, and make sure it's lower case
+    function getPressedKey(event) {
+        // Look for the keyCode of the key being pressed
+        pressedKey = event.keyCode;
+        // If the keyCode is letter...
+        if ((pressedKey >= 65) && (pressedKey <= 90)) {
+            // ...return the letter in lowercase form
+            return (String.fromCharCode(pressedKey)).toLowerCase();
+        }
+        // If the keyCode is a number...
+        else if ((pressedKey >=48) && (pressedKey <= 57)) {
+            // ...return the number as string
+            return (String.fromCharCode(pressedKey))
+        }
+        //
+        else if (pressedKey == 16) {
             //$('.keys').addClass('uppercase');
-        } else {
-            console.log(code);
+        }
+        //
+        else {
+            console.log(pressedKey);
         }
     }
 
     $(document).on({
         // Do this when a key is pressed
         'keydown': function(event) {
-            var key = getCharacter(event);
+            key = getPressedKey(event);
             // Make the on-screen key flash for 100ms
             $('#' + key).addClass('hover');
             // Focus on the textarea
             $('#screen > textarea').focus();
 
-            if (code == 16) {
+            if (pressedKey == 16) {
                 $('.keys').addClass('uppercase');
                 $('#left-shift, #right-shift').addClass('hover');
             }
@@ -77,13 +97,18 @@ $(document).ready(function() {
         },
         // Do this when a key is let go
         'keyup': function(event) {
-            var key = getCharacter(event);
+            key = getPressedKey(event);
             // After a key is clicked, remove the .hover class
-            $('#' + key).removeClass('hover').delay(1000);
-
-            if (code == 16) {
+            setTimeout(function() {
+                $('#' + key).removeClass('hover');
+            }, 100);
+            // If the shift key is let go...
+            if (pressedKey == 16) {
+                // ...remove the .hover and .uppercase classes
+                setTimeout(function() {
+                    $('#left-shift, #right-shift').removeClass('hover');
+                }, 100);
                 $('.keys').removeClass('uppercase');
-                $('#left-shift, #right-shift').removeClass('hover');
             }
         }
     });
