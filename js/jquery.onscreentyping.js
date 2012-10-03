cec791cf4c078b3371b92a2742750e558a18cc32
Brian Fryer <brianfryer@gmail.com>
2012-10-03 13:25:06 -0500

    Capslock & shift keys are working as intended; added "markers" to the F and J keys.

--- js/jquery.onscreentyping.js
+++ js/jquery.onscreentyping.js
@@ -21,34 +21,48 @@ $(document).ready(function() {
         // When a key is pressed ...
         'keydown': function(event) {
 
-            // ... get the pressedKey.
+            // get the pressedKey.
             var pressedKey = getPressedKey(event);
             var pressedKeyChar = pressedKey[0];
             var pressedKeyType = pressedKey[1];
 
-            // ... and make the key hover.
+            // make it hover (unless it's the caps-lock key).
             $('.' + pressedKeyChar).not('.caps-lock').addClass('hover');
 
-            if ((pressedKeyChar == 'caps-lock') && !$('.caps-lock').hasClass('hover')) {
-                $('.caps-lock').addClass('hover');
-                $('.keys').addClass('uppercase');
-            } else {
-                $('.caps-lock').removeClass('hover');
-                $('.keys').removeClass('uppercase');
+            // If the pressedKeyChar is caps-lock...
+            if (pressedKeyChar == 'caps-lock') {
+                // ... make the caps-lock key hover.
+                $('.caps-lock').toggleClass('hover');
+                // ... make the letters uppercase;
+                $('.keys').toggleClass('uppercase');
             }
 
+            // If the pressedKeyChar is shift...
             if (pressedKeyChar == 'shift') {
-                $('.keys').addClass('uppercase');
+                // ... and the caps-lock has hover ...
+                if ($('.caps-lock').hasClass('hover')) {
+                    // ... make the keys lowercase.
+                    $('.keys').removeClass('uppercase');
+                }
+                // ... otherwise ...
+                else {
+                    // ... make the letters uppercase.
+                    $('.keys').addClass('uppercase');
+                }
             }
 
-            if ((pressedKeyChar == 'shift') && $('.caps-lock').hasClass('hover')) {
-                $('.keys').removeClass('uppercase');
-            }
-
-            // If the pressedKey is NOT a modifier key ...
-            if (pressedKeyType !== 'modifier') {
-                // ... focus on the textarea,
+            // If the pressedKey is not a modifier OR is the backspace key...
+            if (pressedKeyType !== 'modifier' || pressedKeyChar == 'backspace') {
+                // focus on the textarea.
                 $('.screen > textarea').focus();
+            }
+
+            // If the pressedKey is a tab...
+            if (pressedKeyChar == 'tab') {
+                // ... prevent its default function.
+                event.preventDefault();
+                // ... insert a tab character at the caret.
+                screen.insertAtCaret('\t');
             }
 
         },
@@ -57,25 +71,34 @@ $(document).ready(function() {
         'keyup': function(event) {
 
             // ... get the (un)pressedKey ...
-            var pressedKey = getPressedKey(event);
-            var pressedKeyChar = pressedKey[0];
-            var pressedKeyType = pressedKey[1];
+            var depressedKey = getPressedKey(event);
+            var depressedKeyChar = depressedKey[0];
+            var depressedKeyType = depressedKey[1];
 
             // ... set a timer for 100ms (to make the key "flash") ...
             setTimeout(function() {
-
                 // ... and remove the hover class from all non-modifier keys (this prevents "sticky hover").
                 $('*').not('.modifier').removeClass('hover');
 
-                // If the (un)pressedKey IS a modifier, and is NOT caps-lock ...
-                if (pressedKeyType == 'modifier') {
-                    // ... remove the hover class of just the modifier key.
-                    $('.' + pressedKeyChar).not('.caps-lock').removeClass('hover');
+                if (depressedKeyType == 'modifier' && depressedKeyChar !== 'caps-lock') {
+                    $('.' + depressedKeyChar).removeClass('hover');
                 }
 
-
-
             }, 100);
+
+            // If the depressedKeyChar is shift...
+            if (depressedKeyChar == 'shift') {
+                // ... and the caps-lock key has hover...
+                if ($('.caps-lock').hasClass('hover')) {
+                    // ... make the letters uppercase.
+                    $('.keys').addClass('uppercase');
+                }
+                // ... otherwise...
+                else {
+                    // ... make the letters lowercase.
+                    $('.keys').removeClass('uppercase');
+                }
+            }
 
         }
     });
@@ -107,54 +130,117 @@ $(document).ready(function() {
             numKey[1] = 'number';
             return numKey;
         }
-        // If the pressedKey is esc
+        // If the pressedKey is an accent, or a tilde ` ~
+        else if (pressedKey == 192) {
+            var accentKey = new Array(2);
+            accentKey[0] = 'accent';
+            accentKey[1] = 'symbol';
+            return accentKey;
+        }
+        // If the pressedKey is a hyphen, or an underscore - _
+        else if (pressedKey == 189) {
+            var hyphenKey = new Array(2);
+            hyphenKey[0] = 'hyphen';
+            hyphenKey[1] = 'symbol';
+            return hyphenKey;
+        }
+        // If the pressedKey is an equals sign, or a plus sign = +
+        else if (pressedKey == 187) {
+            var equalKey = new Array(2);
+            equalKey[0] = 'equal';
+            equalKey[1] = 'symbol';
+            return equalKey;
+        }
+        // If the pressedKey is an opening bracket, or an opening brace [ {
+        else if (pressedKey == 219) {
+            var openingbracketKey = new Array(2);
+            openingbracketKey[0] = 'opening-bracket';
+            openingbracketKey[1] = 'symbol';
+            return openingbracketKey;
+        }
+        // If the pressedKey is a closing bracket, or a closing brace ] }
+        else if (pressedKey == 221) {
+            var closingbracketKey = new Array(2);
+            closingbracketKey[0] = 'closing-bracket';
+            closingbracketKey[1] = 'symbol';
+            return closingbracketKey;
+        }
+        // If the pressedKey is a backslash, or a vertical bar \ |
+        else if (pressedKey == 220) {
+            var backSlashKey = new Array(2);
+            backSlashKey[0] = 'backslash';
+            backSlashKey[1] = 'symbol';
+            return backSlashKey;
+        }
+        // If the pressedKey is a semi-colon, or a colon ; :
+        else if (pressedKey == 186) {
+            var semiColonKey = new Array(2);
+            semiColonKey[0] = 'semi-colon';
+            semiColonKey[1] = 'symbol';
+            return semiColonKey;
+        }
+        // If the pressedKey is an quotation mark, or a double-quotation mark ' "
+        else if (pressedKey == 222) {
+            var quoteKey = new Array(2);
+            quoteKey[0] = 'quote';
+            quoteKey[1] = 'symbol';
+            return quoteKey;
+        }
+        // If the pressedKey is a comma, or a less than sign , <
+        else if (pressedKey == 188) {
+            var commaKey = new Array(2);
+            commaKey[0] = 'comma';
+            commaKey[1] = 'symbol';
+            return commaKey;
+        }
+        // If the pressedKey is a period, or a greater than sign . >
+        else if (pressedKey == 190) {
+            var periodKey = new Array(2);
+            periodKey[0] = 'period';
+            periodKey[1] = 'symbol';
+            return periodKey;
+        }
+        // If the pressedKey is a forward slash, or a question mark / ?
+        else if (pressedKey == 191) {
+            var forwardSlashKey = new Array(2);
+            forwardSlashKey[0] = 'forward-slash';
+            forwardSlashKey[1] = 'symbol';
+            return forwardSlashKey;
+        }
+        // If the pressedKey is the enter key
+        else if (pressedKey == 13) {
+            var enterKey = new Array(2);
+            enterKey[0] = 'enter';
+            enterKey[1] = 'symbol';
+            return enterKey;
+        }
+        // If the pressedKey is the space bar
+        else if (pressedKey == 32) {
+            var spaceBarKey = new Array(2);
+            spaceBarKey[0] = 'space-bar';
+            spaceBarKey[1] = 'symbol';
+            return spaceBarKey;
+        }
+        // If the pressedKey is backspace
+        else if (pressedKey == 8) {
+            var backSpaceKey = new Array(2);
+            backSpaceKey[0] = 'backspace';
+            backSpaceKey[1] = 'symbol';
+            return backSpaceKey;
+        }
+        // If the pressedKey is tab
+        else if (pressedKey == 9) {
+            var tabKey = new Array(2);
+            tabKey[0] = 'tab';
+            tabKey[1] = 'symbol';
+            return tabKey;
+        }
+        // If the pressedKey is escape
         else if (pressedKey == 27) {
             var escapeKey = new Array(2);
             escapeKey[0] = 'escape';
             escapeKey[1] = 'modifier';
             return escapeKey;
-        }
-        // If the pressedKey is an accent `
-        else if (pressedKey == 192) {
-            var accentKey = new Array(2);
-            accentKey[0] = 'accent';
-            accentKey[1] = 'tilde';
-            return accentKey;
-        }
-        // If the pressedKey is a hyphen -
-        else if (pressedKey == 189) {
-            var hyphenKey = new Array(2);
-            hyphenKey[0] = 'hyphen';
-            hyphenKey[1] = 'underscore';
-            return hyphenKey;
-        }
-        // If the pressedKey is an equals sign =
-        else if (pressedKey == 187) {
-            var equalKey = new Array(2);
-            equalKey[0] = 'equal';
-            equalKey[1] = 'plus';
-            return equalKey;
-        }
-        // If the pressedKey is an opening bracket [
-        else if (pressedKey == 219) {
-            var openingbracketKey = new Array(2);
-            openingbracketKey[0] = 'opening-bracket';
-            openingbracketKey[1] = 'opening-brace';
-            return openingbracketKey;
-        }
-        // If the pressedKey is a closing bracket ]
-        else if (pressedKey == 221) {
-            var closingbracketKey = new Array(2);
-            closingbracketKey[0] = 'closing-bracket';
-            closingbracketKey[1] = 'closing-brace';
-            return closingbracketKey;
-        }
-        // If the pressedKey is a backslash \
-        else if (pressedKey == 220) {
-            var backSlashKey = new Array(2);
-            backSlashKey[0] = 'backslash';
-            backSlashKey[1] = 'vertical-bar';
-            return backSlashKey;
         }
         // If the pressedKey is a capsLock
         else if (pressedKey == 20) {
@@ -163,54 +249,12 @@ $(document).ready(function() {
             capsLockKey[1] = 'modifier';
             return capsLockKey;
         }
-        // If the pressedKey is semi-colon ;
-        else if (pressedKey == 186) {
-            var semiColonKey = new Array(2);
-            semiColonKey[0] = 'semi-colon';
-            semiColonKey[1] = 'colon';
-            return semiColonKey;
-        }
-        // If the pressedKey is a quotation mark '
-        else if (pressedKey == 222) {
-            var quoteKey = new Array(2);
-            quoteKey[0] = 'quote';
-            quoteKey[1] = 'double-quote';
-            return quoteKey;
-        }
-        // If the pressedKey is enter
-        else if (pressedKey == 13) {
-            var enterKey = new Array(2);
-            enterKey[0] = 'enter';
-            enterKey[1] = 'modifier';
-            return enterKey;
-        }
         // If the pressedKey is shift
         else if (pressedKey == 16) {
             var shiftKey = new Array(2);
             shiftKey[0] = 'shift';
             shiftKey[1] = 'modifier';
             return shiftKey;
-        }
-        // If the pressedKey is a comma ,
-        else if (pressedKey == 188) {
-            var commaKey = new Array(2);
-            commaKey[0] = 'comma';
-            commaKey[1] = 'less-than-sign';
-            return commaKey;
-        }
-        // If the pressedKey is a period .
-        else if (pressedKey == 190) {
-            var periodKey = new Array(2);
-            periodKey[0] = 'period';
-            periodKey[1] = 'greater-than-sign';
-            return periodKey;
-        }
-        // If the pressedKey is a forward slash /
-        else if (pressedKey == 191) {
-            var forwardSlashKey = new Array(2);
-            forwardSlashKey[0] = 'forward-slash';
-            forwardSlashKey[1] = 'question-mark';
-            return forwardSlashKey;
         }
         // If the pressedKey is ctrl
         else if (pressedKey == 17) {
@@ -225,12 +269,6 @@ $(document).ready(function() {
             altKey[0] = 'alt';
             altKey[1] = 'modifier';
             return altKey;
-        }
-        else if (pressedKey == 32) {
-            var spaceBarKey = new Array(2);
-            spaceBarKey[0] = 'space-bar';
-            spaceBarKey[1] = ' ';
-            return spaceBarKey;
         }
         // If the pressedKey doesn't match anything above, output to the console!
         else {
